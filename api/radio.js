281d4c71d61811c7052b394ab6cfa21d778d2b35
Vv8056 <118241963+Vv8056@users.noreply.github.com>
2026-04-11 00:16:33 +0530

    Update radio.js

--- api/radio.js
+++ api/radio.js
@@ -16,15 +16,39 @@ export default async function handler( req, res) {
     }
 
     try {
-      const { stationid } = req.query;
+      const { stationid, token, lang } = req.query;
 
       if (!stationid) {
-        return res.status(400).json({ error: "stationid required" });
+        return res.status(400).json({ error: "stationid or token required" });
       }
 
-      const data = await api.getRadioStationById({ stationid });
+      // const data = await api.getRadioStationById({ stationid });
+        let data;
 
-      res.setHeader( "Cache-Control", "s-maxage=60" );
+        /* ---------------- TRY FETCH ---------------- */
+
+        if (stationid) {
+          data = await api.getRadioStationById({ stationid });
+        }
+
+        /* ---------------- IF FAILED → RECREATE ---------------- */
+
+        if ( !data || data.error === "No new song found for current radio." ) {
+          if (!token) {
+            return res.status(200).json({ stationid, error: "Station expired — token required to recreate" });
+          }
+            
+          const station = await api.getRadioStation({ token, lang });
+
+          const newStationId = station?.stationid;
+
+          if (!newStationId) {
+            throw new Error( "Failed to create station" );
+          }
+          data = await api.getRadioStationById({ stationid: newStationId });
+        }
+
+      res.setHeader( "Cache-Control", "no-store" );
       res.status(200).json(data);
     }
 
