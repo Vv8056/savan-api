8eb961974fe963ef853dec1a88e64bca940231dd
Vv8056 <118241963+Vv8056@users.noreply.github.com>
2026-06-17 11:22:28 +0530

    Update auto.js

--- api/auto.js
+++ api/auto.js
@@ -6,7 +6,7 @@ const api = new SavanApi();
 
 export default async function handler(req, res) {
 
-  res.setHeader("Access-Control-Allow-Origin", "*"); 
+  res.setHeader("Access-Control-Allow-Origin", "https://jiosavan.pages.dev"); 
   // For production, replace "*" with your domain:
   // res.setHeader("Access-Control-Allow-Origin", "https://yourdomain.com");
 
